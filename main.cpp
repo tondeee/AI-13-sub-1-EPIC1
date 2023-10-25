0373b13b0fe0d412dd23d3a226afcae4fbff02b2
tondeee <85999896+tondeee@users.noreply.github.com>
2023-10-25 21:51:13 +0300

    deposit

--- main.cpp
+++ main.cpp
@@ -4,11 +4,6 @@ using namespace std;
 int main()
 {
     cout << "Dmytro Yunko";
-    cout << "Anastasiia Zhmud";
-    cout << "Mariia Vitkovska";
-    cout << "Anatolii Bachyk";   
-    cout << "Marta Lemishko";
     
-    cout <<"maryna Polishchuk";
     
 }
